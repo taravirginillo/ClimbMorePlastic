3e1ed97c7da84410c87b6a100c8c34d71bbecaeb
taravirginillo <taravirginillo@gmail.com>
2021-02-06 11:48:48 -0800

    added members url and other comments

--- climbMorePlastic.py
+++ climbMorePlastic.py
@@ -9,22 +9,25 @@
 from selenium import webdriver
 import time
 
+# Select proper URL depending on your choice of reservation
+MEMBER_URL = "https://app.rockgympro.com/b/widget/?a=offering&offering_guid=a2015138518d4e389995ee21526c682d&widget_guid=e8cacb9641fa4f6ea12a176adc3a3570&random=601c44ac5b25d&iframeid=rgpiframe601c43cdd5d13&mode=e&course_guid=ca4ec882af24e499d7691d2bf537861b6e519e60"
+NON_MEMBER_URL = "https://app.rockgympro.com/b/widget/?a=offering&offering_guid=fd63ba3934f44647a1151b4ef6507d77&widget_guid=e8cacb9641fa4f6ea12a176adc3a3570&random=601c56694c33c&iframeid=rgpiframe601c43cdd5d13&mode=e&course_guid=ca4ec882966b50e97b44e1832333108e6169537b"
+
 # Connect to chromedriver & load website
-URL = "https://app.rockgympro.com/b/widget/?a=offering&offering_guid=fd63ba3934f44647a1151b4ef6507d77&widget_guid=e8cacb9641fa4f6ea12a176adc3a3570&random=601c56694c33c&iframeid=rgpiframe601c43cdd5d13&mode=e&course_guid=ca4ec882966b50e97b44e1832333108e6169537b"
 CHROMEDRIVER_PATH = r'C:/Program Files/Chromedriver/chromedriver.exe'
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH)
-driver.get(URL)
+driver.get(NON_MEMBER_URL)
 
 # ----------------------------------Global Variables --------------------------------------------------------------------
 # Script not set to accept 2 participants (yet)
 PARTICIPANT_NUM = 1
-WEEK_OF_MONTH = 1
-DAY_OF_WEEK = 7 # Sunday = 1; Monday = 2; Tuesday = 3; Wednesday = 4; etc..
+WEEK_OF_MONTH = 2
+DAY_OF_WEEK = 3 # Sunday = 1; Monday = 2; Tuesday = 3; Wednesday = 4; etc..
 
 # This depends on how many selections are available for that day.
 # Ex: if the first available hour is 12-2pm, then to select the noon slot you set HOUR_OF_DAY = 1
 # On Tuesday for 5:45 pm, it will likely be HOUR_OF_DAY = 3
-HOUR_OF_DAY = 7
+HOUR_OF_DAY = 1
 
 FIRST_NAME = "Tara"
 MIDDLE_NAME = "Christina"
@@ -105,6 +108,7 @@ iagree_1.click()
 iagree_2 = driver.find_element_by_xpath(xpath_iagree_2)
 iagree_2.click()
 
+print("Done third screen. You are not a robot!")
 # You are done the form. Click the "I'm not a bot" and you will be reserved!
 
 
